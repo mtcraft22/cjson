7b336335d5bc444b95e884b48520bca0f0ed5176
mtcraft22 <matort535@gmail.com>
2023-05-25 09:42:26 +0200

    dsffdffdsfdsf

--- structuras_de_datos/dicionaros/preuebadic.c
+++ structuras_de_datos/dicionaros/preuebadic.c
@@ -1,8 +1,8 @@
 #include"dicionarios.h"
 #include<stdio.h>
+#include<stdlib.h>
 
 int main (void){
-
     Lista SO_lin;
 
     Pares win;
@@ -10,21 +10,42 @@ int main (void){
     win.valor="Nueva tecnologia";
     Pares gnu_linux;
     gnu_linux.clave="Gnu_con_linux";
-    gnu_linux.valor="Minix";
+    gnu_linux.valor="minix";
+    Pares macOS;
+    macOS.clave="macOS";
+    macOS.valor="darwin";
     Insertar_pricipio(&SO_lin,&win);
     Insertar_final(&SO_lin,&gnu_linux);
+    Insertar_pricipio(&SO_lin,&macOS);
     void printar(Pares* par){
         printf("Clave: %s, Valor: %s\n", par->clave, par->valor);
     }
 
     por_cada(&SO_lin,printar);
 
-    void* valor2=clave(&SO_lin,"Minix");
+    void* valor2=clave(&SO_lin,"darwin");
     if (valor2!=NULL){
         printf("%s",valor2);
     }else{
         printf("no encontrado el valor");
     }
+
+
+    win.clave="java";
+    por_cada(&SO_lin,printar);
+    void destruir_lista(Lista *lista){
+        Nodo* actual=lista->cabeza;
+        while (actual->siguiente)
+        {
+            free(actual);
+            actual=actual->siguiente;
+        }
+        free(actual);    
+    }
+    destruir_lista(&SO_lin);
+
     return 0;
+    
+    
 
 }
