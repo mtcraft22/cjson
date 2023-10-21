878734934a0e7b9ed96542e4993fd19962b3606f
mtcraft22 <matort535@gmail.com>
2023-10-21 19:52:20 +0200

    dictionaries only delete for implement.

--- structuras_de_datos/dicionarios.c
+++ structuras_de_datos/dicionarios.c
@@ -54,24 +54,37 @@ void debug_map(Persona*map_array[],int array_map_size){
         putc(10, stdout);
     }
 }
-Persona * get_pair (Persona* persona){
-    if(dictlist[hash(persona->nombre)]==persona){
-        return dictlist[hash(persona->nombre)];
+Persona * get_pair (char * clave){
+
+    if(strcmp(dictlist[hash(clave)]->nombre, clave)==0){
+
+        return dictlist[hash(clave)];
     } else{
+        Persona * coso = dictlist[hash(clave)];
+        while (coso!=NULL){
+            if (strcmp(coso->nombre, clave)==0){
+                return coso;
+            }
+            coso=(Persona *)coso->next;
+        }
         return NULL;
     }
 }
 int main(void ){
     Persona a = {.nombre="pepe",.edad=23};
     Persona b = {.nombre="marc",.edad=33};
-    Persona c = {.nombre="jose",.edad=43};
+    Persona c = {.nombre="jose",.edad=93};
     Persona d = {.nombre="luis",.edad=53};
+    Persona e = {.nombre="pepociojose",.edad=76};
     
     insertar(&a); 
     insertar(&b); 
     insertar(&c); 
     insertar(&d);
-   
-    
+    insertar(&e);
+    if( get_pair("pepociojose")){
+        printf("%d\n", get_pair("pepociojose")->edad);
+    }
+
     debug_map(dictlist, hash_size);
 }
